0ae818b3287cdc798a98bc1e94ee60bb2f02c4fe
rya23 <thakur.aarya23@gmail.com>
2026-04-20 13:04:06 +0530

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
